fff428bfd19d9f29cbf4e97be33335111afc9849
goldami1 <goldami1@gmail.com>
2018-05-12 13:27:19 +0300

    initial commit

--- plugin/jiraPlugin/src/main/java/com/atlassian/jiraPlugin/api/MyPluginComponent.java
+++
@@ -1,6 +0,0 @@
-package com.atlassian.jiraPlugin.api;
-
-public interface MyPluginComponent
-{
-    String getName();
-}
---
+++ plugin/statusReminder/src/main/java/com/atlassian/pinkpark/statusReminder/api/MyPluginComponent.java
@@ -0,0 +1,6 @@
+package com.atlassian.pinkpark.statusReminder.api;
+
+public interface MyPluginComponent
+{
+    String getName();
+}
