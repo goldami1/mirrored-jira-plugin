7665ab1fa953a31fb6ad6e8a29a1f493f2235db6
goldami1 <goldami1@gmail.com>
2018-05-20 14:23:47 +0300

    added ability to provide issues according to existing filter
    further info..
    To achieve that, there's a need to provide:
    1. Existing filter id (filter was created through GUI)
    2. User who created the mentioned filter

--- jira-scheduled-mail-plugin/src/main/java/org/mta/oss/impl/MyPluginComponentImpl.java
+++ jira-scheduled-mail-plugin/src/main/java/org/mta/oss/impl/MyPluginComponentImpl.java
@@ -3,13 +3,14 @@ package org.mta.oss.impl;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.IssueManager;
+import com.atlassian.jira.issue.search.SearchException;
 import com.atlassian.jira.issue.search.SearchRequest;
-import com.atlassian.jira.jql.builder.JqlClauseBuilder;
-import com.atlassian.jira.jql.builder.JqlQueryBuilder;
 import com.atlassian.jira.bc.JiraServiceContextImpl;
 import com.atlassian.jira.bc.filter.SearchRequestService;
+import com.atlassian.jira.bc.issue.search.SearchService;
 import com.atlassian.jira.project.Project;
 import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.export.ExportAsService;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.query.Query;
@@ -29,27 +30,29 @@ import javax.inject.Named;
 
 @ExportAsService({MyPluginComponent.class})
 @Named("myPluginComponent")
+@Scanned
 public class MyPluginComponentImpl implements MyPluginComponent {
     private static final Logger log = LoggerFactory.getLogger(MyPluginComponentImpl.class);
 
-    @ComponentImport
     private final ApplicationProperties m_ApplicationProperties;
-    @ComponentImport
     private final IssueManager m_IssueManager;
-
+    private final SearchService m_SearchService;
     private List<Project> m_ProjectsList;
     private List<ApplicationUser> m_NotifiedAdmins;
     private List<Issue> m_FilteredIssuesToBeMailed;
 
     @Inject
-    public MyPluginComponentImpl(final ApplicationProperties i_ApplicationProperties, final IssueManager i_IssueManager)
+    public MyPluginComponentImpl(	@ComponentImport final ApplicationProperties i_ApplicationProperties,
+    								@ComponentImport final IssueManager i_IssueManager,
+    								@ComponentImport final SearchService i_SearchService)
     {
-    	Collection<Long> issuesIds = null;
-        List<Issue> issuesList = null;
-        
-        m_NotifiedAdmins = new LinkedList<>();
         m_ApplicationProperties = i_ApplicationProperties;
         m_IssueManager = i_IssueManager;
+        m_SearchService = i_SearchService;
+        
+    	Collection<Long> issuesIds = null;
+        List<Issue> issuesList = null;
+        m_NotifiedAdmins = new LinkedList<>();
         m_ProjectsList = ComponentAccessor.getProjectManager().getProjects();
         
         
@@ -60,7 +63,7 @@ public class MyPluginComponentImpl implements MyPluginComponent {
         
 		try
 		{
-			issuesIds = i_IssueManager.getIssueIdsForProject(m_ProjectsList.get(0).getId());
+			issuesIds = m_IssueManager.getIssueIdsForProject(m_ProjectsList.get(0).getId());
 		} catch (GenericEntityException e)
 		{
 			e.printStackTrace();
@@ -68,13 +71,44 @@ public class MyPluginComponentImpl implements MyPluginComponent {
 		
 		if(issuesIds!=null)
 		{
-			issuesList = i_IssueManager.getIssueObjects(issuesIds);
+			issuesList = m_IssueManager.getIssueObjects(issuesIds);
 		}
 		
 		
 		//<begin> gets filter with id=10003 and associated to ApplicationUser m_NotifiedAdmins.get(0). This filter was created through Jira's GUI
 		SearchRequestService srs = ComponentAccessor.getComponent(SearchRequestService.class);
 		SearchRequest sr = srs.getFilter(new JiraServiceContextImpl(m_NotifiedAdmins.get(0)), Long.valueOf(10003));
+		Query srq = sr.getQuery();
+		String generatedJqlString = m_SearchService.getGeneratedJqlString(srq);
+		final SearchService.ParseResult parseResult = m_SearchService.parseQuery(m_NotifiedAdmins.get(0), generatedJqlString);
+		
+		if(parseResult.isValid())
+		{
+			try
+			{
+				com.atlassian.jira.issue.search.SearchResults results = m_SearchService.search(m_NotifiedAdmins.get(0), parseResult.getQuery(), com.atlassian.jira.web.bean.PagerFilter.getUnlimitedFilter());
+				m_FilteredIssuesToBeMailed = results.getIssues();
+				
+				for(Issue i : m_FilteredIssuesToBeMailed)
+				{
+					String issueDescReflect = i.getDescription();
+					issueDescReflect+=" ";
+				}
+				
+			}catch(SearchException ex)
+			{
+				ex.printStackTrace();
+			}
+		}
+		else
+		{
+			String errorMsg = "";
+			for(String str : parseResult.getErrors().getErrorMessages())
+			{
+				errorMsg += " " + str;
+			}
+			(new Exception(errorMsg)).printStackTrace();
+		}
 		//<end>
 		
 		
@@ -82,7 +116,8 @@ public class MyPluginComponentImpl implements MyPluginComponent {
     }    
     
 
-	public String getName() {
+	public String getName()
+	{
         if (null != m_ApplicationProperties) {
             return "myComponent:" + m_ApplicationProperties.getDisplayName();
         }
